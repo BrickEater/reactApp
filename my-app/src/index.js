fa6e3172a029b55ba8e221861d9d83f539cc5972
Cody <codyratcliffep@gmail.com>
2022-09-28 15:15:09 -0600

    a small amount of work done on this tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,13 +10,14 @@ class Square extends React.Component {
             value: null,
         };
     }
-/* https://reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment just finished this part above to remember the state of the board */
 
     render() {
       return (
-        <button className="square" onClick={() =>
-          console.log('click')}>
-          {this.props.value}
+        <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
